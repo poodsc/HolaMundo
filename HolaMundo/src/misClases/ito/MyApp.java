473c3636c3e7e0ab5e80a320c189b2ea600c5ade
rherrera <rherrera@rherrera-PC>
2021-02-17 20:15:12 -0600

    El metodo main se actualizo por segunda vez.

--- HolaMundo/src/misClases/ito/MyApp.java
+++ HolaMundo/src/misClases/ito/MyApp.java
@@ -5,6 +5,8 @@ public class MyApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
+		System.out.println("Como estan?");
+		System.out.println("Espero que todo muy bien");
 
 	}
 
